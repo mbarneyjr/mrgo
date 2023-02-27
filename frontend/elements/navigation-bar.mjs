b4046a23ed014d57c83835e93e6db97a693dc5ea
Michael Barney, Jr <mbarneyme@gmail.com>
2023-02-26 23:43:53 -0500

    cleanup nav bar css

--- frontend/elements/navigation-bar.mjs
+++ frontend/elements/navigation-bar.mjs
@@ -9,36 +9,34 @@ export function element({ html, state }) {
   const currentPath = state.store?.path;
   const isAuthenticated = state.store?.session?.idToken && state.store?.session?.accessToken && state.store?.session?.refreshToken;
   const authLink = isAuthenticated
-    ? /* html */ `<a class="auth-link" href="/logout">Log Out</a>`
-    : /* html */ `<a class="auth-link" href="/login">Log In</a>`;
+    ? /* html */ `<a class="${ELEMENT_NAME}-link" href="/logout">Log Out</a>`
+    : /* html */ `<a class="${ELEMENT_NAME}-link" href="/login">Log In</a>`;
   return html`
     <style>
-      ${ELEMENT_NAME} #navigation {
+      .${ELEMENT_NAME}-nav {
         width: 100%;
         display: flex;
         flex-wrap: wrap;
         background-color: ${config.colors.primary};
       }
-      ${ELEMENT_NAME} a {
-        text-decoration: none;
-        color: black;
+      .${ELEMENT_NAME}-link {
+        all: unset;
+        cursor: pointer;
         padding: 1rem;
+        flex-basis: auto;
         background-color: ${config.colors.primary};
       }
-      ${ELEMENT_NAME} a:hover {
+      .${ELEMENT_NAME}-link:hover {
         background-color: ${config.colors.primaryHeavy};
       }
-      .auth-link {
-        margin-left: auto;
-      }
-      ${ELEMENT_NAME} a.active-nav {
+      .${ELEMENT_NAME}-active-nav {
         background-color: ${config.colors.background};
       }
     </style>
-    <nav id="navigation">
-      <a href="/" class="${currentPath === '/' ? 'active-nav' : ''}">Go Home</a>
-      <a href="/urls" class="${currentPath.includes('/urls') ? 'active-nav' : ''}">View URLs</a>
-      <a href="/create-url" class="${currentPath.includes('/create-url') ? 'active-nav' : ''}">Create URL</a>
+    <nav id="navigation" class="${ELEMENT_NAME}-nav">
+      <a class="${ELEMENT_NAME}-link ${currentPath === '/' ? `${ELEMENT_NAME}-active-nav` : ''}" href="/">Go Home</a>
+      <a class="${ELEMENT_NAME}-link ${currentPath.includes('/urls') ? `${ELEMENT_NAME}-active-nav` : ''}" href="/urls">View URLs</a>
+      <a class="${ELEMENT_NAME}-link ${currentPath.includes('/create-url') ? `${ELEMENT_NAME}-active-nav` : ''}" href="/create-url">Create URL</a>
       ${authLink}
     </nav>
   `;

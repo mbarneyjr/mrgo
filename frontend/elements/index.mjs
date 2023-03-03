733fb70657b9f5f3484b9164f122698bf2d98f0d
Michael Barney, Jr <mbarneyme@gmail.com>
2023-03-02 21:55:31 -0500

    adds mrgo robot

--- frontend/elements/index.mjs
+++ frontend/elements/index.mjs
@@ -1,9 +1,11 @@
+import * as MrgoRobot from './mrgo-robot.mjs';
 import * as CreateUrlForm from './create-url-form.mjs';
 import * as EditUrlForm from './edit-url-form.mjs';
 import * as UrlList from './url-list.mjs';
 import * as NavigationBar from './navigation-bar.mjs';
 
 export default {
+  [MrgoRobot.ELEMENT_NAME]: MrgoRobot.element,
   [CreateUrlForm.ELEMENT_NAME]: CreateUrlForm.element,
   [EditUrlForm.ELEMENT_NAME]: EditUrlForm.element,
   [UrlList.ELEMENT_NAME]: UrlList.element,

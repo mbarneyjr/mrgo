ca0d5a7776a804020692f274e5bc2a071d6f1f0d
Michael Barney, Jr <mbarneyme@gmail.com>
2023-04-23 16:34:27 -0400

    better logging and config validation

--- frontend/lib/auth/index.mjs
+++ frontend/lib/auth/index.mjs
@@ -1,42 +1,47 @@
 import fetch from 'node-fetch';
 
 import { config } from '../config/index.mjs';
+import { logger } from '../logger/index.mjs';
 
 /** @type {import('./index.js').getTokens} */
 export async function getTokens(_, authCode) {
+  const tokenParams = {
+    client_id: config.auth.clientId,
+    scope: config.auth.scope,
+    redirect_uri: `${config.appEndpoint}/oauth2/idresponse`,
+    grant_type: 'authorization_code',
+    code: authCode,
+  };
+  logger.debug('getting tokens', { tokenParams });
   const response = await fetch(`${config.auth.baseUrl}/oauth2/token`, {
     method: 'POST',
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
     },
-    body: new URLSearchParams({
-      client_id: config.auth.clientId,
-      scope: config.auth.scope,
-      redirect_uri: `${config.appEndpoint}/oauth2/idresponse`,
-      grant_type: 'authorization_code',
-      code: authCode,
-    }),
+    body: new URLSearchParams(tokenParams),
   });
   const body = /** @type {import('./index.js').TokenCodeResponse} */ (await response.json());
   if (response.status < 200 || 299 < response.status) {
-    throw new Error(`failed to fetch tokens, ${body.error}`);
+    throw new Error(`failed to fetch tokens: ${body.error}`);
   }
   return body;
 }
 
 /** @type {import('./index.js').refreshTokens} */
 export async function refreshTokens(refreshToken) {
+  const refreshTokenParams = {
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken,
+    client_id: config.auth.clientId,
+    scope: config.auth.scope,
+  };
+  logger.debug('refreshing tokens', { refreshTokenParams });
   const response = await fetch(`${config.auth.baseUrl}/oauth2/token`, {
     method: 'POST',
     headers: {
       'content-type': 'application/x-www-form-urlencoded',
     },
-    body: new URLSearchParams({
-      grant_type: 'refresh_token',
-      refresh_token: refreshToken,
-      client_id: config.auth.clientId,
-      scope: config.auth.scope,
-    }),
+    body: new URLSearchParams(refreshTokenParams),
   });
   const body = /** @type {import('./index.js').RefreshedTokenCodeResponse} */ (await response.json());
   if (response.status < 200 || 299 < response.status) {
--- frontend/lib/config/index.mjs
+++ frontend/lib/config/index.mjs
@@ -1,14 +1,27 @@
+if (!process.env.APP_CLIENT_ID) {
+  throw new Error('APP_CLIENT_ID is not defined');
+}
+if (!process.env.AUTH_BASE_URL) {
+  throw new Error('AUTH_BASE_URL is not defined');
+}
+if (!process.env.API_ENDPOINT) {
+  throw new Error('API_ENDPOINT is not defined');
+}
+if (!process.env.APP_ENDPOINT) {
+  throw new Error('APP_ENDPOINT is not defined');
+}
+
 export const config = {
   auth: {
-    clientId: `${process.env.APP_CLIENT_ID}`,
+    clientId: process.env.APP_CLIENT_ID,
     scope: 'openid email',
-    baseUrl: `${process.env.AUTH_BASE_URL}`,
+    baseUrl: process.env.AUTH_BASE_URL,
   },
   api: {
     pageSize: 10,
   },
-  apiEndpoint: `${process.env.API_ENDPOINT}`,
-  appEndpoint: `${process.env.APP_ENDPOINT}`,
+  apiEndpoint: process.env.API_ENDPOINT,
+  appEndpoint: process.env.APP_ENDPOINT,
   /** @type {import('./index.js').ColorConfig} */
   colors: {
     nav: {
